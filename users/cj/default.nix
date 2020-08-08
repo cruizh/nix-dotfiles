3dfc7443b3bb02fdb49cc77ae43ffed1a224fb27
Carlos José Ruiz-Henestrosa Ruiz <ruizh.cj@gmail.com>
2020-08-08 13:52:08 +0200

    Set name and hashedPassword

--- users/cj/default.nix
+++ users/cj/default.nix
@@ -5,6 +5,7 @@
     imports = [ ../profiles/lorri
                 ../profiles/kitty
                 ../profiles/kitty/themes/dracula
+                ../profiles/starship
               ];
 
     programs.git.enable = true;
@@ -17,6 +18,7 @@
     nixpkgs.config.allowUnfree = true;
     home.packages = with pkgs; [
       bitwarden bitwarden-cli
+      tdesktop
       spotify
       discord
       skypeforlinux
@@ -28,8 +30,8 @@
 
   users.users.cj = {
     uid = 1000;
-    initialPassword = "nixos";
-    description = "default";
+    hashedPassword = "$6$ww9uUxCwTf7tkV$7wKhIAVSR2it93yaa3KBqwxR0dGlXlAbHpDvitTOOwHTDIjW01WpV0AOz4bgwUwj54bV4SO/afEg54hNrhQ2w1";
+    description = "Carlos José Ruiz-Henestrosa Ruiz";
     isNormalUser = true;
     extraGroups = [ "wheel" ];
   };
