dd9114a08794a46d9d16f030a59aa87b87bb056c
Carlos José Ruiz-Henestrosa Ruiz <ruizh.cj@gmail.com>
2020-08-19 18:09:52 +0200

    Generalized rclone service

--- users/cj/rclone.nix
+++ users/cj/rclone.nix
@@ -1,27 +1,27 @@
 { pkgs, config, ... }:
 
 let
-  mountdir = "${config.home.homeDirectory}/Sync/OneDrive";
+  mountdir = "${config.home.homeDirectory}/Sync";
 in
 {
   home.packages = [ pkgs.rclone ];
   home.file.".config/rclone/rclone.conf".source = ../../secrets/rclone.conf;
 
   systemd.user.services = {
-    rclone-onedrive = {
+    "rclone@" = {
       Unit.Description = "Mount OneDrive";
       Unit.After = [ "network-online.target" ];
       Install.WantedBy = [ "multi-user.target" ];
       Service = {
-        ExecStartPre = "/run/current-system/sw/bin/mkdir -p ${mountdir}";
+        ExecStartPre = "/run/current-system/sw/bin/mkdir -p ${mountdir}/%i";
         ExecStart = ''
-          ${pkgs.rclone}/bin/rclone mount us-onedrive: ${mountdir} \
+          ${pkgs.rclone}/bin/rclone mount %i: "${mountdir}/%i" \
           --vfs-cache-max-age 48h \
           --vfs-cache-mode full \
           --dir-cache-time 48h
         '';
-        ExecStop = "/run/wrappers/bin/fusermount -u ${mountdir}";
-        ExecStopPost = "/run/current-system/sw/bin/rmdir ${mountdir}";
+        ExecStop = "/run/wrappers/bin/fusermount -u \"${mountdir}/%i\"";
+        ExecStopPost = "/run/current-system/sw/bin/rmdir \"${mountdir}/%i\"";
         Type = "notify";
         Restart = "on-failure";
         RestartSec = "10s";
